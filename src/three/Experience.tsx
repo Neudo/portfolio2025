dce542a4050120753d9cc593829e1a1374e725e1
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2025-07-01 14:29:00 +0200

    feat: start rotation path

--- src/three/Experience.tsx
+++ src/three/Experience.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, Suspense, useEffect, useRef, useState } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import {
   Environment,
@@ -37,13 +37,59 @@ const Scene = ({
 }) => {
   const cameraCurve = new THREE.CatmullRomCurve3([
     new THREE.Vector3(-46, 22, -15),
-    new THREE.Vector3(12, 7.4, -16),
+    new THREE.Vector3(12, 7.4, -16), // About
     new THREE.Vector3(30, 10, -16),
     new THREE.Vector3(40, 10, -5),
     new THREE.Vector3(40, 10, 15),
-    new THREE.Vector3(33, 10, 20),
-    new THREE.Vector3(10, 10, 20),
+    new THREE.Vector3(33, 10, 20), // skills
+    new THREE.Vector3(16, 10, 20), // Projects
+    new THREE.Vector3(-1, 10, 20), // Contact
   ]);
+
+  const rotationTargets = [
+    {
+      progress: 0,
+      rotation: new THREE.Euler(-2.16, -1.046, -2.23),
+    },
+    {
+      progress: 0.13,
+      rotation: new THREE.Euler(0.0068, -0.15, 0.001),
+    },
+    {
+      progress: 0.33,
+      rotation: new THREE.Euler(-2.87, -0.53, -3.0),
+    },
+    {
+      progress: 0.78,
+      rotation: new THREE.Euler(0.34, -1.42, 0.34),
+    },
+    {
+      progress: 0.87,
+      rotation: new THREE.Euler(0.13, -0.39, 3.14),
+    },
+    {
+      progress: 1,
+      rotation: new THREE.Euler(2.9, -0.12, 3.11),
+    },
+  ];
+
+  const getLerpedRotation = (progress: number) => {
+    for (let i = 0; i < rotationTargets.length - 1; i++) {
+      const start = rotationTargets[i];
+      const end = rotationTargets[i + 1];
+
+      if (progress >= start.progress && progress <= end.progress) {
+        const lerpFactor =
+          (progress - start.progress) / (end.progress - start.progress);
+
+        return new THREE.Euler(
+          THREE.MathUtils.lerp(start.rotation.x, end.rotation.x, lerpFactor),
+          THREE.MathUtils.lerp(start.rotation.y, end.rotation.y, lerpFactor),
+          THREE.MathUtils.lerp(start.rotation.z, end.rotation.z, lerpFactor)
+        );
+      }
+    }
+  };
 
   useFrame(() => {
     if (camera && targetScrollProgress) {
@@ -57,6 +103,12 @@ const Scene = ({
 
       const point = cameraCurve.getPoint(newProgress);
       camera.current.position.copy(point);
+
+      const targetRotation = getLerpedRotation(newProgress);
+      camera.current.rotation.copy(targetRotation);
+
+      console.log("Rotation->>>>", camera.current.rotation);
+      console.log("Progress ----->", newProgress);
     }
   });
 
@@ -84,6 +136,7 @@ export default function Experience() {
   });
   const lerpFactor = controls.lerpFactor;
   const scrollSpeed = controls.scrollSpeed;
+  const isSwiping = useRef(false);
 
   useEffect(() => {
     const handleWheel = (e: WheelEvent) => {
@@ -91,10 +144,31 @@ export default function Experience() {
         targetScrollProgress.current + Math.sign(e.deltaY) * scrollSpeed;
     };
 
+    const handlePointerDown = () => {
+      isSwiping.current = true;
+    };
+
+    const handlePointerMove = (e: PointerEvent) => {
+      if (!isSwiping.current) return;
+      targetScrollProgress.current =
+        targetScrollProgress.current +
+        Math.sign(e.movementY) * scrollSpeed * 0.2;
+    };
+
+    const handlePointerUp = () => {
+      isSwiping.current = false;
+    };
+
     window.addEventListener("wheel", handleWheel, { passive: false });
+    // window.addEventListener("pointerdown", handlePointerDown);
+    // window.addEventListener("pointermove", handlePointerMove);
+    // window.addEventListener("pointerup", handlePointerUp);
 
     return () => {
       window.removeEventListener("wheel", handleWheel);
+      window.removeEventListener("pointerdown", handlePointerDown);
+      window.removeEventListener("pointermove", handlePointerMove);
+      window.removeEventListener("pointerup", handlePointerUp);
     };
   }, []);
 
