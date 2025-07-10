b8776d06a04f8327a9155308d5d73ea5e1ca2ec5
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2025-07-10 12:14:21 +0200

    fix: responsive

--- src/components/layout/ui/SceneUi.tsx
+++ src/components/layout/ui/SceneUi.tsx
@@ -5,7 +5,7 @@ import Credits from "@/components/Modals/Credits";
 export default function SceneUi() {
   const { setTheme, openModal } = useModalStore();
   return (
-    <div className="fixed top-4 right-4 z-30 flex gap-2">
+    <div className="fixed md:top-4 md:right-4 md:bottom-[unset] bottom-4 right-4 z-30 flex gap-2">
       <button
         className="bg-slate-200 px-4 py-2 rounded-md cursor-pointer"
         onClick={() => setTheme("day")}
