740e08aa292404876a3ce76363ac6aa53530b810
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2025-10-01 16:47:24 +0200

    remove hover image

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -1,25 +1,13 @@
 import { motion } from "motion/react";
 type Props = {
   title: string;
-  description: string;
   preview: string;
-  color: string;
   url: string;
   index: number;
   style?: string;
-  tags?: string[];
 };
 
-export default function WorkCard({
-  title,
-  description,
-  preview,
-  color,
-  url,
-  index,
-  style,
-  tags,
-}: Props) {
+export default function WorkCard({ title, preview, url, index, style }: Props) {
   return (
     <motion.div
       className={`relative w-full overflow-hidden mb-8 md:mb-0 group ${index === 2 ? "" : ""}`}
@@ -41,32 +29,6 @@ export default function WorkCard({
             alt="#"
             className="object-cover rounded-sm w-full h-full"
           />
-        </div>
-        <div
-          className={`absolute top-[-100%] group-hover:top-0 hidden md:flex transition-all duration-350 ease-out rounded-sm left-0 w-full h-full  p-4 flex-col ${style !== "modal" ? "justify-between" : "justify-center"}`}
-          style={{ backgroundColor: color }}
-        >
-          {style !== "modal" && (
-            <>
-              <p className="text-slate-200 text-xl group-hover:opacity-100 opacity-0 delay-300 transition-opacity pr-4">
-                {description}
-              </p>
-              {tags && (
-                <div className="flex-1 mt-3 ">
-                  {tags?.map((tag) => {
-                    return (
-                      <p className="bg-black mr-2 text-slate-200 text-xs py-0.5 px-2 rounded-md w-fit inline-flex items-center justify-center">
-                        {tag}
-                      </p>
-                    );
-                  })}
-                </div>
-              )}
-            </>
-          )}
-          <div className="px-4 py-2 font-bold bg-slate-50  hover:bg-slate-200 transition-all inline-block rounded-md text-center max-w-[320px] mx-auto">
-            Découvrir
-          </div>
         </div>
       </a>
       <motion.h3
