1abbb8bf494592bc9db522cc855e8a963263ad06
Neudo <bassalair.quentin@gmail.com>
2026-02-11 11:37:52 +0100

    feat: wording

--- src/components/Modals/About.tsx
+++ src/components/Modals/About.tsx
@@ -1,7 +1,10 @@
 export default function About() {
   return (
     <>
-      <p>Je m'appelle Quentin Bassalair et je suis développeur web frontend.</p>
+      <p>
+        Je m'appelle Quentin Bassalair et je suis développeur full stack (node
+        js, react, php).
+      </p>
       <p>
         Après cinq ans à l'armée de terre, j'ai repris des études en tant que
         développeur web et mobile en 2020 à Paris.
@@ -10,7 +13,6 @@ export default function About() {
         Lors de ma seconde année scolaire, j'ai été pris en alternance, puis en
         CDI dans une agence digital en qualité de développeur fullstack.
       </p>
-      <p>Je travaillais principalement sur PHP, JavaScript, React.</p>
     </>
   );
 }
